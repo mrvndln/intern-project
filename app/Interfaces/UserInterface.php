7e03f9cd4de80ac4bebe942d4a3e5754011c5c4a
markadrianogay <markadrianogay@gmail.com>
2025-04-06 09:52:15 +0800

    Add update and delete

--- app/Interfaces/UserInterface.php
+++ app/Interfaces/UserInterface.php
@@ -6,5 +6,6 @@ interface UserInterface{
     public function getAll();
     public function add($data);
     public function update($data,$id);
+    public function delete($id);
     public function find($data);
 }

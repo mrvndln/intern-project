bc48940f7bd0670160d19c1d1fcd4fa875ae9b7f
markadrianogay <markadrianogay@gmail.com>
2025-04-06 09:47:56 +0800

    Add logout button

---
+++ resources/views/livewire/logout-user.blade.php
@@ -0,0 +1,4 @@
+<div class="flex justify-center flex-col items-center">
+    <h1 class="text-white">Home</h1>
+    <button wire:click="logout" class="bg-red-500 text-white py-1 px-2 hover:bg-red-400 rounded-sm">Logout</button>
+</div>

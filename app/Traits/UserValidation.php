3fe8d74ec6639ec4aa2cc451717a89bfb92a6d03
markadrianogay <markadrianogay@gmail.com>
2025-04-12 14:05:44 +0800

    Add validation rules messages method

--- app/Traits/UserValidation.php
+++ app/Traits/UserValidation.php
@@ -16,4 +16,31 @@ trait UserValidation
       'password' => 'required|min:6',
     ];
   }
+
+  public function validation_rules_messages()
+  {
+    return [
+      'name.required' => 'Please fill out this field.',
+      'name.min' => 'The name must be at least 6 characters.',
+
+      'contact.required' => 'Please fill out this field.',
+      'contact.min' => 'The contact must be at least 11 digits.',
+      'contact.unique' => 'This contact number is already taken.',
+
+      'email.required' => 'Please fill out this field.',
+      'email.email' => 'Please enter a valid email address.',
+
+      'address.required' => 'Please fill out this field.',
+      'address.min' => 'The address must be at least 10 characters.',
+
+      'birthdate.required' => 'Please fill out this field.',
+
+      'username.required' => 'Please fill out this field.',
+      'username.min' => 'The username must be at least 6 characters.',
+      'username.unique' => 'This username is already taken.',
+
+      'password.required' => 'Please fill out this field.',
+      'password.min' => 'The password must be at least 6 characters.',
+    ];
+  }
 }

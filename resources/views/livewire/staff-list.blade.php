402e53b86b8431d4dccfe56cc2d8c068b17061d3
markadrianogay <markadrianogay@gmail.com>
2025-04-12 13:29:29 +0800

    Add staff list blade template

---
+++ resources/views/livewire/staff-list.blade.php
@@ -0,0 +1,35 @@
+<div>
+    <div wire:click="$dispatch('openModal',{component: 'add-user', params: []})" class="flex justify-end p-2">
+        <button class="w-sm flex items-center justify-center px-4 py-2 text-sm text-white bg-green-500 hover:bg-green-600 rounded-lg transition-colors"><i class="fas fa-user-plus mr-3 text-gray-100"></i>Add Users</button>
+    </div>
+    
+    <table class="min-w-full border">
+        <thead>
+            <tr class="bg-gray-200">
+                <th class="py-2 px-4 border">Name</th>
+                <th class="py-2 px-4 border">Contact</th>
+                <th class="py-2 px-4 border">Email</th>
+                <th class="py-2 px-4 border">Address</th>
+                <th class="py-2 px-4 border">Birthdate</th>
+                <th class="py-2 px-4 border">Username</th>
+                <th class="py-2 px-4 border">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($users as $user)
+            <tr>
+                <td class="py-2 px-4 border">{{ $user->name }}</td>
+                <td class="py-2 px-4 border">{{ $user->contact }}</td>
+                <td class="py-2 px-4 border">{{ $user->email }}</td>
+                <td class="py-2 px-4 border">{{ $user->address }}</td>
+                <td class="py-2 px-4 border">{{ $user->birthdate }}</td>
+                <td class="py-2 px-4 border">{{ $user->username }}</td>
+                <td class="py-2 px-4 border">
+                    <button wire:click="editUser({{ $user->id }})" class="text-blue-500 hover:text-blue-700">Edit</button>
+                        <button wire:click="triggerDelete({{ $user->id }})" type="button" class="text-red-500 hover:text-red-700 ml-2">Delete</button>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>

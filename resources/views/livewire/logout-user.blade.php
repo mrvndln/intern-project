6565404c0bbbc509377c972de28db457fec7026c
markadrianogay <markadrianogay@gmail.com>
2025-04-12 13:30:34 +0800

    Fix button template

--- resources/views/livewire/logout-user.blade.php
+++ resources/views/livewire/logout-user.blade.php
@@ -1,4 +1,4 @@
-<div class="flex justify-center flex-col items-center">
-    <h1 class="text-white text-7xl mt-5">Home</h1>
-    <button wire:click="logout" class="bg-red-500 text-white py-1 px-2 hover:bg-red-400 rounded-sm mt-8">Logout</button>
-</div>
+<button wire:click="logout" class="p-2 rounded-full text-gray-500 hover:bg-gray-100 focus:outline-none">
+    <i class="fas fa-sign-out-alt mr-2"></i>
+    Logout
+</button>

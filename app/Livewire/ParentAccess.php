ccb7f070cb3f48e7de51e1c82687c39a8921953d
markadrianogay <markadrianogay@gmail.com>
2025-04-28 18:47:56 +0800

    Add  parent access component class

---
+++ app/Livewire/ParentAccess.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Traits\BootUserRepository;
+use Livewire\Component;
+
+class ParentAccess extends Component
+{
+
+    use BootUserRepository;
+
+    protected $repository;
+    public $moduleName;
+    public $suggestions;
+
+    public function suggestModule()
+    {
+        $this->reset(['suggestions']);
+        $this->suggestions = $this->repository->findModule($this->moduleName);
+    }
+
+    public function updateOrSave()
+    {
+        $this->repository->updateOrCreate($this->moduleName);
+        $this->dispatch('show-success-modal', message: 'Module Saved or Updated Successfully!');
+        $this->dispatch('closeModal');
+    }
+
+    public function searchField($data) {    
+        $this->moduleName = $data;
+        $this->reset('suggestions');
+    }
+
+    public function render()
+    {
+        return view('livewire.parent-access');
+    }
+}

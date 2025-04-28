fba17857f16d3a00627fe05297fa045a8c21c782
markadrianogay <markadrianogay@gmail.com>
2025-04-28 18:51:50 +0800

    Add access control

--- resources/views/livewire/access-control.blade.php
+++ resources/views/livewire/access-control.blade.php
@@ -1,29 +1,20 @@
-<div wire:cloak class="bg-white rounded-lg p-10 w-[45rem] h-[20rem]">
-    <div class="mb-2 flex justify-center items-center flex-col">
-        <div class="font-bold text-2xl mb-4 font-mono">Access Control</div>
-        <div class="w-full">
-            <div class="h-[10rem]">
-                <input wire:model="moduleName" wire:keydown="suggestModule" name="query" type="search"
-                    class="border-2 w-full rounded-md h-9 shadow-sm px-3 @error('name') border-red-500 @enderror"
-                    placeholder="Search" autocomplete="off">
-
-                        @if($suggestions !== null && $moduleName !== '')
-                            @foreach($suggestions as $suggestion)
-                                 <div wire:click="" class="px-3 mt-1">{{ $suggestion->module_name }}</div> 
-                            @endforeach
-                        @endif
-
-            </div>
-
-            <div class="flex justify-end space-x-3">
-                <button wire:click="$dispatch('closeModal')" type="button" value="cancel" class="px-4 py-2 bg-gray-200 rounded-md hover:bg-gray-300 transition-colors text-center">
-                    Cancel
-                </button>
-                <button type="button" wire:click="updateOrSave" class="px-4 py-2 bg-amber-400 rounded-md hover:bg-amber-300 transition-colors">
-                    Save
-                </button>
-            </div>
-        </div>
-    </div>
-
+<div wire:cloak>
+    <table class="min-w-full border">
+        <thead>
+            <tr class="bg-gray-200">
+                <th class="py-2 px-4 border">Roles</th>
+                <th class="py-2 px-4 border">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($roles as $role)
+            <tr>
+                <td class="py-2 px-4 border">{{ $role->role }}</td>
+                <td class="py-2 px-4 border">
+                    <button wire:click="$dispatch('openModal', { component: 'edit-role', params: [] })" class="text-blue-500 hover:text-blue-700"> <i class="fa fa-edit"></i></button>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>

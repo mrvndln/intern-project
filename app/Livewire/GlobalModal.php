1992aa01bc24daf657b7db56e0343b4cfc01c4ad
markadrianogay <markadrianogay@gmail.com>
2025-06-04 14:57:11 +0800

    Change parent acess property and event-listeners to addPermissions

--- app/Livewire/GlobalModal.php
+++ app/Livewire/GlobalModal.php
@@ -8,8 +8,12 @@ use Livewire\Component;
 class GlobalModal extends Component
 {
     public $show = false;
+    public $parentAccess = false;
+    public $create_modal = false;
+    public $edit_modal = false;
     public $component = '';
-    public $params = [];  
+    public $user_id;  
+    public $params = [];
     
     #[On('openModal')]
     public function openModal($component, $params) {
@@ -18,12 +22,38 @@ class GlobalModal extends Component
         $this->show = true;
     }
 
+    #[On('create_modal')]
+    public function open_create_modal(){
+        $this->create_modal = true;
+    }
+
+    #[On('edit_modal')]
+    public function open_edit_modal($params){
+        $this->params = $params;
+
+        if($this->params){
+            $this->edit_modal = true;
+        }
+       
+    }
+
     #[On('closeModal')]
     public function closeModal() {
         $this->dispatch('reload-list');
         $this->reset();
-        $this->show = false;
+        $this->create_modal = false;
     }
+
+    #[On('open-parent-access')]
+    public function openParentAccess() {
+        $this->parentAccess = true;
+    }
+
+    #[On('close-parent-access')]
+    public function closeParentAccess() {
+        $this->parentAccess = false;
+    }
+
     public function render()
     {
         return view('livewire.global-modal');

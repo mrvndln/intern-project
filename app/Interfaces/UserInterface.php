e9ec118d50918038786037f73e0de14898b63819
markadrianogay <markadrianogay@gmail.com>
2025-06-04 14:24:45 +0800

    Add addtional interfaces

--- app/Interfaces/UserInterface.php
+++ app/Interfaces/UserInterface.php
@@ -6,11 +6,17 @@ interface UserInterface{
     public function getAll();
     public function getRoles();
     public function add($data);
+
     public function update($data,$id);
     public function updateOrCreate($data);
     public function getModules();
+
     public function findModule($data);
     public function delete($id);
     public function find($data);
-    public function getResults($data);
+    
+    public function addRolePermission($selectedPermissions, $roleId);
+    public function getRolePermissions($roleId);
+
+    public function getResults($data, $searchId);
 }

d4ad22018fb76bf52bfacf849eb145e2b25a7df6
markadrianogay <markadrianogay@gmail.com>
2025-06-04 15:55:32 +0800

    Entangle alpine properties to livewire

--- resources/views/livewire/global-modal.blade.php
+++ resources/views/livewire/global-modal.blade.php
@@ -1,7 +1,36 @@
-<div x-data={show:@entangle('show')}>
-    <div x-show="show" class = "fixed inset-0 bg-black/50 flex justify-center items-center" >
+<div x-data="{ 
+               show:         @entangle('show'),
+               addPermissions: @entangle('addPermissions'),
+               create_modal: @entangle('create_modal'),
+               edit_modal:   @entangle('edit_modal')
+}">
+
+
+    <div x-show="show" class="fixed inset-0 bg-black/50 flex justify-center items-center">
         @if($component)
             @livewire($component, $params)
         @endif
     </div>
+
+    <div x-show="addPermissions" class="fixed inset-0 bg-black/50 flex justify-center items-center">
+        @if($addPermissions)
+            @livewire('parent-access', $params)
+        @endif
+    </div>
+
+    <x-modal id="create_modal">
+        <x-slot name="title">Add User</x-slot>
+        <x-slot name="body">
+            @livewire('add-user')
+        </x-slot>
+    </x-modal>
+
+    <x-modal id="edit_modal">
+        <x-slot name="title">Update User</x-slot>
+        <x-slot name="body">
+            @if($params)
+                @livewire('update-user', $params)
+            @endif
+        </x-slot>
+    </x-modal>
 </div>

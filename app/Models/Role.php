bbdb3d2323bab4485c046af3bc762104c717d7e4
markadrianogay <markadrianogay@gmail.com>
2025-06-04 15:22:13 +0800

    Add hasMany and  belongsToMany relationship

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,13 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
     use HasFactory;
 
-    public function users(): BelongsToMany {
-        return $this->belongsToMany(User::class);
-    }    
+    public function users(): HasMany {
+        return $this->hasMany(User::class);
+    }  
+    
+
+    public function permissions(): BelongsToMany {
+        return $this->belongsToMany(Permission::class,'role_permissions');
+    }
 }

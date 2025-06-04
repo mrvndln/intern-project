5032a8eacef2e01b203ff8ce5d9ee35acd1d8490
markadrianogay <markadrianogay@gmail.com>
2025-06-04 15:26:31 +0800

    Register dynamic gates for permissions

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\Permission;
+use Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +24,15 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Permission::all()->each(function ($permission) {
+            Gate::define($permission->access_module_name, function ($user) use ($permission) {
+                return optional($user->role)
+                    ->permissions
+                    ->pluck('access_module_name')
+                    ->contains($permission->access_module_name);
+            });
+        });
     }
 }

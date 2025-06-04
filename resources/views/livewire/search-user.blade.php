d7a3602e270c7d6a22bd06ae754b8437a75e1a7f
markadrianogay <markadrianogay@gmail.com>
2025-06-04 16:06:55 +0800

    Remove @error

--- resources/views/livewire/search-user.blade.php
+++ resources/views/livewire/search-user.blade.php
@@ -1,6 +1,5 @@
 <div class="w-80">
     <input wire:model="searchInput" wire:keydown="search" name="query" type="search"
-        class="border-2 w-full rounded-md h-9 shadow-sm px-3 @error('name') border-red-500 @enderror"
+        class="border-2 w-full rounded-md h-9 shadow-sm px-3 focus:outline-gray-300 @error('name') border-red-500 @enderror"
         placeholder="Search">
-    @error('name') <span class="text-red-500 text-sm">{{ $message }}</span> @enderror
 </div>

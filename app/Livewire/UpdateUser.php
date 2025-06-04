c22d51ec52ad192938032516efb786fb983d0284
markadrianogay <markadrianogay@gmail.com>
2025-06-04 15:19:53 +0800

    Add null coalescing operator in properties

--- app/Livewire/UpdateUser.php
+++ app/Livewire/UpdateUser.php
@@ -7,17 +7,18 @@ use App\Traits\UserValidation;
 use Livewire\Component;
 
 class UpdateUser extends Component
-{   
+{
     use BootUserRepository;
     use UserValidation;
 
     public $id;
-    public $name, $contact, $email, $address, $birthdate, $username, $password, $current_role, $roles, $role_id;
+    public $name, $contact, $email, $address, $birthdate, $username, $password, $currentRole_id, $current_role, $roles, $role_id;
     protected $repository;
 
-    public function mount(){
+    public function mount()
+    {
         $this->roles = $this->repository->getRoles();
-        $this->editUser($this->id); 
+        $this->editUser($this->id);
     }
 
     protected function rules()
@@ -28,25 +29,24 @@ class UpdateUser extends Component
     public function editUser($id)
     {
         $user = $this->repository->find($id);
-        $this->id = $user->id;
-        $this->name = $user->name;
-        $this->contact = $user->user_detail->contact;
-        $this->email = $user->email;
-        $this->address = $user->user_detail->address;
-        $this->birthdate = $user->user_detail->birthdate;
-        $this->username = $user->username;
         
-        foreach($user->roles as $role){
-            $this->role_id = $role->id;
-            $this->current_role = $role->role;
-        }
+        $this->id = $user->id ?? "";
+        $this->name = $user->name ?? "";
+        $this->contact = $user->contact ?? "";
+        $this->email = $user->email ?? "";
+        $this->address = $user->address ?? "";
+        $this->birthdate = $user->birthdate ?? "";
+        $this->username = $user->username ?? "";
+        $this->current_role = $user->role ?? "";
+        $this->role_id = $user->role_id ?? "";
+        
     }
 
     public function updateUser()
     {
 
         $validated = $this->validate();
-
+        
         $this->repository->update($validated, $this->id);
         $this->dispatch('show-success-modal', message: 'User updated successfully!');
         $this->dispatch('reload-list');

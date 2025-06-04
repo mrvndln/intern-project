aaf633f14e50fa3fadf4c727a27c0c328f7a16ab
markadrianogay <markadrianogay@gmail.com>
2025-06-04 16:04:16 +0800

    Add loading

--- resources/views/livewire/logout-user.blade.php
+++ resources/views/livewire/logout-user.blade.php
@@ -1,4 +1,5 @@
 <button wire:click="logout" class="p-2 rounded-full text-gray-500 hover:bg-gray-100 focus:outline-none">
     <i class="fas fa-sign-out-alt mr-2"></i>
     Logout
+    <x-loading target="logout"/>
 </button>

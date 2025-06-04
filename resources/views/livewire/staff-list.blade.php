9cfbb11a22f93ce5beea75b7db69dd1321a4ff4c
markadrianogay <markadrianogay@gmail.com>
2025-06-04 16:05:35 +0800

    Use @can for access control

--- resources/views/livewire/staff-list.blade.php
+++ resources/views/livewire/staff-list.blade.php
@@ -1,12 +1,16 @@
 <div wire:cloak>
-
     <div class="flex items-center w-full p-2">
-        <livewire:search-user>
-        <div class="flex justify-end w-full ">
-            <button wire:click="$dispatch('openModal',{component: 'add-user', params: []})" class="w-sm flex items-center justify-center px-4 py-2 text-sm text-white bg-green-500 hover:bg-green-600 rounded-lg transition-colors"><i class="fas fa-user-plus mr-3 text-gray-100"></i>Add Users</button>
-        </div>
-    </div>
+         <x-search  wire:model="searchInput" wire:keydown="search" name="query" type="search"
+        class="border-2 w-full rounded-md h-9 shadow-sm px-3 focus:outline-gray-300"
+        placeholder="Search" />
 
+            <div class="flex justify-end w-full gap-1">
+                @can('manage-roles')
+                    <button @click="$dispatch('manage_roles', { value: 'manageRoles' })" class="w-sm flex items-center justify-center px-4 py-2 text-sm text-white bg-blue-500 hover:bg-blue-600 rounded-md transition-colors"><i class="fas fa-user-gear mr-3 text-gray-100"></i>Manage Roles</button>
+                @endcan
+                <button @click="$dispatch('create_modal')" class="w-sm flex items-center justify-center px-4 py-2 text-sm text-white bg-green-500 hover:bg-green-600 rounded-md transition-colors"><i class="fas fa-user-plus mr-3 text-gray-100"></i>Add User</button>
+            </div>
+    </div>  
 
     <table class="min-w-full border">
         <thead>
@@ -22,25 +26,26 @@
             </tr>
         </thead>
         <tbody>
-            @foreach($users as $user)
+            @forelse($users as $user)
             <tr>
                 <td class="py-2 px-4 border">{{ $user->name }}</td>
-                <td class="py-2 px-4 border">
-                    @foreach($user->roles as $role)
-                    {{ $role->role }}{{ !$loop->last ? ', ' : '' }}
-                    @endforeach
-                </td>
-                <td class="py-2 px-4 border">{{ $user->user_detail->contact }}</td>
+                <td class="py-2 px-4 border">{{ $user->role }}</td>
+                <td class="py-2 px-4 border">{{ $user->contact }}</td>
                 <td class="py-2 px-4 border">{{ $user->email }}</td>
-                <td class="py-2 px-4 border">{{ $user->user_detail->address }}</td>
-                <td class="py-2 px-4 border">{{ $user->user_detail->birthdate }}</td>
+                <td class="py-2 px-4 border">{{ $user->address }}</td>
+                <td class="py-2 px-4 border">{{ $user->birthdate }}</td>
                 <td class="py-2 px-4 border">{{ $user->username }}</td>
                 <td class="py-2 px-4 border">
-                    <button wire:click="$dispatch('openModal',{component: 'update-user', params: { id: {{ $user->id }} } })" class="text-blue-500 hover:text-blue-700"> <i class="fa fa-edit"></i></button>
+                    <button wire:click="$dispatch('edit_modal',{ params: { id: {{ $user->id }} } })" class="text-blue-500 hover:text-blue-700"> <i class="fa fa-edit"></i></button>
                     <button wire:click="triggerDelete({{ $user->id }})" type="button" class="text-red-500 hover:text-red-700 ml-2"> <i class="fa fa-trash"></i></button>
                 </td>
             </tr>
-            @endforeach
+            @empty
+            <tr>
+                <td>NO RESULT</td>
+            </tr>
+            @endforelse
         </tbody>
     </table>
+    <x-loading target="deleteUser"/>
 </div>

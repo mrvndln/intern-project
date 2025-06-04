cbf9817d50d5d550a6c2f712319abd1c0d7af538
markadrianogay <markadrianogay@gmail.com>
2025-06-04 14:28:42 +0800

    Use event listener for opening role management and role permisions

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -1,13 +1,25 @@
 <?php
 
 namespace App\Livewire;
-
-use App\Repositories\UserRepository;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public $activeView = 'dashboard';
+
+    #[On('manage_roles')]
+    public function openManageRoles($value)
+    {
+        $this->activeView = $value;
+    }
+
+    #[On('role_permissions')]
+    public function openRolePermissions($value)
+    {
+        $this->activeView = $value;
+    }
+
     public function render()
     {
         return view('livewire.dashboard');

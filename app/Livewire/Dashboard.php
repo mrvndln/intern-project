7f02f7e38e98e787daba0b2c67a6633759330679
markadrianogay <markadrianogay@gmail.com>
2025-06-05 16:39:21 +0800

    Add interface totalUsers and activeUsers properties

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -1,12 +1,23 @@
 <?php
 
 namespace App\Livewire;
+
+use App\Traits\BootUserRepository;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    use BootUserRepository;
+
     public $activeView = 'dashboard';
+    public $totalUsers, $activeUsers;
+
+    public function mount()
+    {
+        $this->totalUsers = $this->repository->totalUsers();
+        $this->activeUsers = $this->repository->activeUsers();
+    }
 
     #[On('manage_roles')]
     public function openManageRoles($value)

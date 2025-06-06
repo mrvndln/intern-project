4e21ec2663b605ed9fc9e02b1f0fc96bb5801c6e
markadrianogay <markadrianogay@gmail.com>
2025-06-06 16:28:27 +0800

    Use ValidationTrait

--- app/Livewire/ParentAccess.php
+++ app/Livewire/ParentAccess.php
@@ -3,35 +3,42 @@
 namespace App\Livewire;
 
 use App\Traits\BootUserRepository;
+use App\Traits\UserValidation;
+use App\Traits\ValidationTrait;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class ParentAccess extends Component
 {
 
     use BootUserRepository;
+    use ValidationTrait;
 
     protected $repository;
-    public $moduleName;
+
     public $suggestions;
+    public $moduleName;
+   
+
+    protected function rules()
+    {
+       return $this->validation_rules_array('searchPermssions'); // this will return an array of validation rules.
+    }
 
     public function suggestModule()
     {
         $this->reset(['suggestions']);
+        $this->resetValidation();
         $this->suggestions = $this->repository->findModule($this->moduleName);
     }
 
     public function updateOrSave()
     {
-        if(empty($this->moduleName)){
-
-            
-        }else{
-            $this->repository->updateOrCreate($this->moduleName);
-            $this->dispatch('show-success-modal', message: 'Permission has been successfully added/updated!');
-            $this->dispatch('close-parent-access');
-            $this->dispatch('hide-permission-edit');
-        }
-       
+        $validated = $this->validate();
+        $this->repository->updateOrCreate($validated['moduleName']);
+        $this->dispatch('show-success-modal', message: 'Permission has been successfully added/updated!');
+        $this->dispatch('close-add-permissions');
+        $this->dispatch('hide-permission-edit');
     }
 
     public function searchField($data) {    

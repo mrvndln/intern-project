4e1ab4f4103d66fdb38dcf70c29aae5685a0f444
markadrianogay <markadrianogay@gmail.com>
2025-06-16 19:15:02 +0800

    Add new modals for patient management

--- resources/views/livewire/global-modal.blade.php
+++ resources/views/livewire/global-modal.blade.php
@@ -2,7 +2,9 @@
                show:         @entangle('show'),
                addPermissions: @entangle('addPermissions'),
                create_modal: @entangle('create_modal'),
-               edit_modal:   @entangle('edit_modal')
+               edit_modal:   @entangle('edit_modal'),
+               editPatient_modal:   @entangle('editPatient_modal'),
+               add_patient: @entangle('add_patient')
 }">
 
 
@@ -14,10 +16,10 @@
 
     <div x-show="addPermissions" class="fixed inset-0 bg-black/50 flex justify-center items-center">
         @if($addPermissions)
-            @livewire('parent-access', $params)
+            @livewire('parent-access', $params) 
         @endif
     </div>
-
+    
     <x-modal id="create_modal">
         <x-slot name="title">Add User</x-slot>
         <x-slot name="body">
@@ -25,12 +27,31 @@
         </x-slot>
     </x-modal>
 
-    <x-modal id="edit_modal">
-        <x-slot name="title">Update User</x-slot>
+    <x-modal id="add_patient">
+        <x-slot name="title">Add Patient</x-slot>
         <x-slot name="body">
-            @if($params)
-                @livewire('update-user', $params)
-            @endif
+            @livewire('add-patient')
         </x-slot>
     </x-modal>
+
+    <x-modal id="edit_modal">
+    <x-slot name="title">Update User</x-slot>
+    <x-slot name="body">
+        @if($params)
+            @livewire('update-user', $params, key('update-user-' . ($params['id'] ?? uniqid())))
+        @endif
+    </x-slot>
+</x-modal>
+
+<x-modal id="editPatient_modal">
+    <x-slot name="title">Update Patient</x-slot>
+    <x-slot name="body">
+        @if($params)
+            @livewire('update-patient', $params, key('update-patient-' . ($params['id'] ?? uniqid())))
+        @endif
+    </x-slot>
+</x-modal>
+
+
+    
 </div>

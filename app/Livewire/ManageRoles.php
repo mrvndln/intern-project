a13673ad1ec71a30ce6e75234635f09f86286af3
markadrianogay <markadrianogay@gmail.com>
2025-06-16 18:56:22 +0800

    Use BootTrait and UserRepository

--- app/Livewire/ManageRoles.php
+++ app/Livewire/ManageRoles.php
@@ -1,26 +1,29 @@
 <?php
 
 namespace App\Livewire;
+
+use App\Traits\BootTrait;
 use App\Traits\BootUserRepository;
+use App\Traits\BootUserTrait;
 use Livewire\Component;
 
 class ManageRoles extends Component
 {
-    use BootUserRepository;
-
+    use BootTrait;
+    
     protected $repository;
     public $searchInput;
     public $roles;
 
     public function mount()
     {
-        $this->roles = $this->repository->getRoles();
+        $this->roles = $this->user_repo->getRoles();
     }
 
     public function search()
     {
        $data = $this->searchInput;
-       $this->roles = $this->repository->getResults($data,'role-list');
+       $this->roles = $this->user_repo->getResults($data,'role-list');
     }
 
 

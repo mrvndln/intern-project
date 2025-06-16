44090f432e7d429735414811fbe9deee3fee4e17
markadrianogay <markadrianogay@gmail.com>
2025-06-16 18:55:05 +0800

    Use BootTrait and UserRepository

--- app/Livewire/EditRole.php
+++ app/Livewire/EditRole.php
@@ -2,32 +2,34 @@
 
 namespace App\Livewire;
 
+use App\Traits\BootTrait;
 use App\Traits\BootUserRepository;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
 class EditRole extends Component
 {
-    use BootUserRepository;
+    use BootTrait;
 
     public $data, $searchData, $modules, $roles;
 
     public function mount()
     {
-        $this->modules = $this->repository->getModules();
-        $this->roles = $this->repository->getRoles();
+        $this->modules = $this->user_repo->getModules();
+        $this->roles = $this->user_repo->getRoles();
     }
 
     #[On('close-parent-access')]
     public function reloadModuleList() 
     {
-        $this->modules = $this->repository->getModules();
+        $this->modules = $this->user_repo->getModules();
     }
 
     public function searchModule()
     {
-        $this->modules = $this->repository->findModule($this->searchData);
+        $this->modules = $this->user_repo->findModule($this->searchData);
     }
+
     public function render()
     {
         return view('livewire.edit-role');

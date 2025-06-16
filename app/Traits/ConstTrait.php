e3675b586ba04dbc46905a39cd3a9317abb7c133
markadrianogay <markadrianogay@gmail.com>
2025-06-16 19:11:08 +0800

    Create ConstTrait

---
+++ app/Traits/ConstTrait.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Traits;
+
+
+trait ConstTrait
+{
+  const TYPE_USER = 'user';
+  
+  const TYPE_PATIENT = 'patient';
+
+  const ACTION_CREATE = 'create';
+
+  const ACTION_UPDATE = 'update';
+
+}
